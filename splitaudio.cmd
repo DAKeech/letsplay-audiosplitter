0dfef8788b3c6c0bfb2a79dd85272e6e99e2a05c
Douglas Keech <DAKeech@users.noreply.github.com>
2016-10-02 18:16:08 -0500

    Update splitaudio.cmd

--- splitaudio.cmd
+++ splitaudio.cmd
@@ -1,8 +1,10 @@
 @echo off
 
+@REM Change this path.
+set exepath=..\ffmpeg
+
 set InputVid=%1%
 set DeskOut=%InputVid%-Desktop.aac
 set VocalOut=%InputVid%-Vocal.aac
 
-
-..\ffmpeg -i %InputVid% -map 0:a:0 -codec copy %DeskOut% -map 0:a:1 -codec copy %VocalOut%
+%exepath% -i %InputVid% -map 0:a:0 -codec copy %DeskOut% -map 0:a:1 -codec copy %VocalOut%
